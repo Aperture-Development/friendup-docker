02b3249061781349c1a8b81281e6e2af84c9d69b
Maximilian Grüttemeier <flisim2004@outlook.de>
2020-01-27 16:03:41 +0100

    Bugfix
    Fixed #2 and #1
    additionally implemented security check if the database container has successfully started

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+# Check if database is ready
+RESULT="FirstStart"
+until [ "$RESULT" == "mysqld is alive" ]
+do
+    echo "MariaDB didn't finish starting yet"
+    sleep 5s
+    RESULT=$(mysqladmin ping -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -h friendup-db --protocol=tcp)
+done
+
 # Check if Database Exists, if not create it
-RESULT=`mysqlshow --host=friendup-mariadb --user=root --password=$MYSQL_ROOT_PASSWORD friendup| grep -v Wildcard | grep -o friendup`
-if [ "$RESULT" == "friendup" ]; then
-    mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -hfriendup-mariadb -e "CREATE DATABASE $MYSQL_DATABASE;CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; GRANT ALL PRIVILEGES ON '$MYSQL_DATABASE'.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;"
-    mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -hfriendup-mariadb < /friendup/db/FriendCoreDatabase.sql
+RESULT=$(echo '$(mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -hfriendup-db --protocol=tcp -D"$MYSQL_DATABASE" -Bse 'SHOW TABLES' -D"$MYSQL_DATABASE")' | wc -l)
+if [ "$RESULT" == "1" ]; then
+    mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -D"$MYSQL_DATABASE" -hfriendup-db --protocol=tcp < /friendup/db/FriendCoreDatabase.sql
 fi
+echo "$RESULT"
 
 # Check if the config FIle already exists, if not create it
 if [ -f '/friendup/build/cfg/cfg.ini' ]; then
@@ -14,7 +23,7 @@ if [ -f '/friendup/build/cfg/cfg.ini' ]; then
 else
     touch /friendup/build/cfg/cfg.ini
     echo '[DatabaseUser]' >> /friendup/build/cfg/cfg.ini
-    echo 'host=friendup-mariadb' >> /friendup/build/cfg/cfg.ini
+    echo 'host=friendup-db' >> /friendup/build/cfg/cfg.ini
     echo "login=$MYSQL_USER" >> /friendup/build/cfg/cfg.ini
     echo "password=$MYSQL_PASSWORD" >> /friendup/build/cfg/cfg.ini
     echo "dbname=$MYSQL_DATABASE" >> /friendup/build/cfg/cfg.ini
