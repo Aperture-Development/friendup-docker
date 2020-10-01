0a4a3cf8bab8756b75675f7289a695963e3bce13
Maximilian Grüttemeier <flisim2004@outlook.de>
2020-10-02 00:47:34 +0200

    Manual setup
    - Add ability to manually run FriendUP without docker compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,24 +8,29 @@ if [ -d '/friendup/build/storage' ]; then
     exit 1
 fi
 
+# Check if MYSQL_HOST enviroment variable is empty, if yes set it to default value
+if [ -z "$MYSQL_HOST" ]; then
+    MYSQL_HOST="friendup-db"
+fi
+
 # Check if database is ready
-RESULT="FirstStart"
+RESULT=$(mysqladmin ping -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -h"$MYSQL_HOST" --protocol=tcp)
 until [ "$RESULT" == "mysqld is alive" ]
 do
-    echo "[ENTRY] MariaDB didn't finish starting yet"
+    echo "[INFO] Could not connect to Database, retrying in 10 seconds"
     sleep 10s
-    RESULT=$(mysqladmin ping -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -h friendup-db --protocol=tcp)
+    RESULT=$(mysqladmin ping -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -h"$MYSQL_HOST" --protocol=tcp)
 done
 
 # Check if Database Exists, if not create it
-RESULT=$(echo $(mysql -u"$MYSQL_USER" -hfriendup-db --protocol=tcp -D"$MYSQL_DATABASE" -Bse 'SHOW TABLES' -D"$MYSQL_DATABASE" | wc -l))
+RESULT=$(echo $(mysql -u"$MYSQL_USER" -h"$MYSQL_HOST" --protocol=tcp -D"$MYSQL_DATABASE" -Bse 'SHOW TABLES' -D"$MYSQL_DATABASE" | wc -l))
 if [ "$RESULT" == "0" ]; then
-    echo '[ENTRY] Creating Database'
-    mysql -u"$MYSQL_USER" -D"$MYSQL_DATABASE" -hfriendup-db --protocol=tcp < /opt/friendup/db/FriendCoreDatabase.sql
+    echo '[INFO] Creating Database'
+    mysql -u"$MYSQL_USER" -D"$MYSQL_DATABASE" -h"$MYSQL_HOST" --protocol=tcp < /opt/friendup/db/FriendCoreDatabase.sql
 
     for i in $(ls -v /opt/friendup/sqlupdatescripts/); do
-        echo "[ENTRY] Running $i"
-        mysql -u"$MYSQL_USER" -D"$MYSQL_DATABASE" -hfriendup-db --protocol=tcp < /opt/friendup/sqlupdatescripts/"$i"
+        echo "[INFO] Running $i"
+        mysql -u"$MYSQL_USER" -D"$MYSQL_DATABASE" -h"$MYSQL_HOST" --protocol=tcp < /opt/friendup/sqlupdatescripts/"$i"
     done;
 fi
 
@@ -33,7 +38,7 @@ fi
 if [ ! -f '/opt/friendup/cfg/cfg.ini' ]; then
     touch /opt/friendup/cfg/cfg.ini
     echo '[DatabaseUser]'                   >> /opt/friendup/cfg/cfg.ini
-    echo 'host=friendup-db'                 >> /opt/friendup/cfg/cfg.ini
+    echo "host=$MYSQL_HOST"                 >> /opt/friendup/cfg/cfg.ini
     echo "login=$MYSQL_USER"                >> /opt/friendup/cfg/cfg.ini
     echo "password=$MYSQL_PASSWORD"         >> /opt/friendup/cfg/cfg.ini
     echo "dbname=$MYSQL_DATABASE"           >> /opt/friendup/cfg/cfg.ini
@@ -52,7 +57,7 @@ if [ ! -f '/opt/friendup/cfg/cfg.ini' ]; then
     echo ''                                 >> /opt/friendup/cfg/cfg.ini
     echo '[FriendChat]'                     >> /opt/friendup/cfg/cfg.ini
     echo 'enabled = 0'                      >> /opt/friendup/cfg/cfg.ini
-    echo '[ENTRY] Created new config file'
+    echo '[INFO] Created new config file'
 fi
 
 # Finish up and start Friend
