87d14d17bf0da5b2b5af9afcc3c563f58e076a07
Maximilian Grüttemeier <flisim2004@outlook.de>
2020-11-04 02:28:54 +0100

    Buxfix & Versioning
    - Introduce versioning for docker-compose file
    - Add check for SSL certificate expiration
    - Fix sed replace from regex to litteral

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 export MYSQL_PWD="$MYSQL_PASSWORD"
 
+# Version of the dockerfile
+FRND_CONTAINER_VERSION="1"
+
 # Check for old file structure
-if [ -d '/friendup/build/storage' ]; then
-    echo "[ERROR] Old filestructure found, please get the new version of the docker-compose.yml file here:"
+if [[ ! "$FRND_CONTAINER_VERSION" == "$FRND_DOCKER_VERSION" ]]; then
+    echo "[ERROR] Old docker-compose.yml version found! Please update the docker-compose file from this url:"
     echo "https://github.com/Aperture-Development/friendup-docker"
     exit 1
 fi
@@ -34,22 +37,27 @@ if [ "$RESULT" == "0" ]; then
     done;
 fi
 
+# SSL Certificate
 if [ ! -f '/opt/friendup/cfg/crt/key.pem' ]; then
     # generate new ssl certificate
+    echo '[INFO] Generating new SSL certificate'
     openssl req -nodes -sha512 -x509 -days 3650 -newkey rsa:2048 -keyout /opt/friendup/cfg/crt/key.pem -out /opt/friendup/cfg/crt/certificate.pem -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=$DOCKER_FRIEND_DOMAIN"
 else
-    # generate csr from existing key
-    openssl req -new -sha512 -x509 -nodes -days 3650 -key /opt/friendup/cfg/crt/key.pem -out /opt/friendup/cfg/crt/certificate.pem -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=$DOCKER_FRIEND_DOMAIN"
+    # Check if certificate will expire within the next 30 days and renew it if yes
+    if ! openssl x509 -checkend 2592000 -noout -in /opt/friendup/cfg/crt/certificate.pem; then
+        echo '[INFO] Updating SSL certificate'
+        openssl req -new -sha512 -x509 -nodes -days 3650 -key /opt/friendup/cfg/crt/key.pem -out /opt/friendup/cfg/crt/certificate.pem -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=$DOCKER_FRIEND_DOMAIN"
+    fi
 fi
 
 # Check if the config File already exists, if not create it
 if [ ! -f '/opt/friendup/cfg/cfg.ini' ]; then
     cp /opt/friendup/cfg/cfg.ini.sample /opt/friendup/cfg/cfg.ini
-    sed -i "s/#DB_USER#/$MYSQL_USER/" /opt/friendup/cfg/cfg.ini
-    sed -i "s/#DB_PASS#/$MYSQL_PASSWORD/" /opt/friendup/cfg/cfg.ini
-    sed -i "s/#DB_HOST#/$MYSQL_HOST/" /opt/friendup/cfg/cfg.ini
-    sed -i "s/#DB_NAME#/$MYSQL_DATABASE/" /opt/friendup/cfg/cfg.ini
-    sed -i "s/#FRND_DOMAIN#/$DOCKER_FRIEND_DOMAIN/" /opt/friendup/cfg/cfg.ini
+    sed -i "s|#DB_USER#|$MYSQL_USER|g" /opt/friendup/cfg/cfg.ini
+    sed -i "s|#DB_PASS#|$MYSQL_PASSWORD|g" /opt/friendup/cfg/cfg.ini
+    sed -i "s|#DB_HOST#|$MYSQL_HOST|g" /opt/friendup/cfg/cfg.ini
+    sed -i "s|#DB_NAME#|$MYSQL_DATABASE|g" /opt/friendup/cfg/cfg.ini
+    sed -i "s|#FRND_DOMAIN#|$DOCKER_FRIEND_DOMAIN|g" /opt/friendup/cfg/cfg.ini
     echo '[INFO] Created new config file'
 fi
 
